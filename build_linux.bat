3019b26a42e19b283bea39ae9be7fdc01aca1948
GraceLiu <784139247@qq.com>
2020-01-05 22:10:22 +0800

    update

--- build_linux.bat
+++ build_linux.bat
@@ -1,4 +1,9 @@
+cd ..
+cd ..
+set GOPATH=%cd%
 set GOOS=linux
 set GOPACH=amd64
-go build -tags="jsoniter" -o deploy/server server/server.go
-go build -tags="jsoniter" -o deploy/client client/client.go
+cd src
+cd httpproxy.v1
+go build -tags="jsoniter" -o deploy/bin/server ./server
+go build -tags="jsoniter" -o deploy/bin/client ./client
